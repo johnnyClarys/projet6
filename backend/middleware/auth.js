4b310a9d2a5bf1feeef0f1f9553465dd578f0d4c
Johnny Clarys <johnny.Clarys@hotmail.fr>
2024-07-20 07:45:10 +0200

    edit middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,21 +1,26 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User')
+const User = require('../models/User');
 const { secretKey } = require('../config');
 
 module.exports = async (req, res, next) => {
-    try{
+    try {
+        // Récupère le token JWT de l'en-tête d'autorisation
         const token = req.headers.authorization.split(' ')[1];
+        // Vérifie et décode le token JWT
         const decodedToken = jwt.verify(
             token,
             secretKey
         );
+        // Récupère l'ID de l'utilisateur à partir du token décodé
         const userId = decodedToken.userID;
 
+        // Recherche l'utilisateur par ID dans la base de données
         const user = await User.findById(userId);
         if (!user) {
             return res.status(401).json({ error: 'User not found. Please log in again.' });
         }
 
+        // Vérifie l'inactivité de l'utilisateur
         const currentTime = Date.now();
         const lastActivity = user.lastActivity.getTime();
         const maxDelay = 10 * 60 * 1000; // 10 minutes
@@ -25,14 +30,17 @@ module.exports = async (req, res, next) => {
             return res.status(401).json({ error: 'Session expired due to inactivity. Please log in again.' });
         }
 
+        // Met à jour la dernière activité de l'utilisateur
         user.lastActivity = Date();
         await user.save();
 
+        // Ajoute l'ID de l'utilisateur à l'objet de requête pour un accès ultérieur
         req.auth = {
             userId: userId
         }
         next();
-    } catch(error) {
+    } catch (error) {
+        // Gère les erreurs de token JWT
         if (error instanceof jwt.TokenExpiredError) {
             res.status(401).json({ error: 'JWT token has expired. Please log in again.' });
         } else if (error instanceof jwt.JsonWebTokenError) {
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,25 +1,32 @@
 const multer = require('multer');
 
-// Define allowed MIME types and their corresponding file extensions
+// Définit les types MIME autorisés et leurs extensions de fichier correspondantes
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png',
 };
 
+// Utilise le stockage en mémoire pour multer
 const storage = multer.memoryStorage();
 
+// Filtre les fichiers selon leur type MIME
 const fileFilter = (req, file, cb) => {
+  // Vérifie si le type MIME du fichier est autorisé
   if (MIME_TYPES[file.mimetype]) {
+    // Accepte le fichier
     cb(null, true);
   } else {
+    // Refuse le fichier avec une erreur
     cb(new Error('Le type du fichier est incorrect'), false);
   }
 };
 
+// Configure multer avec le stockage en mémoire et le filtre de type de fichier
 const upload = multer({ 
   storage: storage, 
   fileFilter: fileFilter 
 }).single('image');
 
+// Exporte le middleware multer configuré
 module.exports = upload;
