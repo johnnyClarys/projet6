023f276fac716fb059ff608721f3f4316e34e081
Johnny Clarys <johnny.Clarys@hotmail.fr>
2024-07-20 07:48:20 +0200

    edit app.js

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const path = require('path');
-const connectDB = require('./db');
-const corsConfig = require('./corsConfig');
+const connectDB = require('./db'); // Fonction pour se connecter à la base de données MongoDB
+const corsConfig = require('./corsConfig'); // Configuration des en-têtes CORS
 
-const bookRoutes = require('./routes/book');
-const userRoutes = require('./routes/user');
+const bookRoutes = require('./routes/book'); // Routes pour les livres
+const userRoutes = require('./routes/user'); // Routes pour les utilisateurs
 
-const app = express();
+const app = express(); // Création de l'application Express
 
-// Connect to MongoDB
+// Connexion à MongoDB
 connectDB();
 
-// CORS Middleware
+// Middleware pour la gestion des en-têtes CORS
 app.use(corsConfig);
 
-// Parse JSON bodies
+// Middleware pour analyser les corps de requêtes JSON
 app.use(express.json());
 
-// Routes
-app.use('/api/books', bookRoutes);
-app.use('/api/auth', userRoutes);
+// Routes pour les API
+app.use('/api/books', bookRoutes); // Routes pour les livres
+app.use('/api/auth', userRoutes);  // Routes pour l'authentification des utilisateurs
+
+// Route pour servir les images statiques depuis le dossier 'images'
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-
+// Exportation de l'application Express pour l'utiliser dans le serveur principal
 module.exports = app;
