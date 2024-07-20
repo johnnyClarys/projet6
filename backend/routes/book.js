56264647a2e4d9da7c92c2525577d47089ae43c8
Johnny Clarys <johnny.Clarys@hotmail.fr>
2024-07-20 07:47:40 +0200

    edit Routes

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,16 +1,25 @@
 const express = require('express');
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
-const router = express.Router();
+const auth = require('../middleware/auth'); // Middleware d'authentification
+const multer = require('../middleware/multer-config'); // Middleware pour la gestion des fichiers
+const router = express.Router(); // Création du routeur Express
 
-const { getAllBook, getBestBooks, getOneBook, createBook, modifyBook, deleteBook, createRatingBook } = require('../controllers/book')
+// Importation des fonctions du contrôleur de livre
+const { getAllBook, getBestBooks, getOneBook, createBook, modifyBook, deleteBook, createRatingBook } = require('../controllers/book');
 
+// Route pour obtenir les livres avec la meilleure note
 router.get('/bestrating', getBestBooks);
+// Route pour obtenir un livre par son ID
 router.get('/:id', getOneBook);
+// Route pour obtenir tous les livres
 router.get('/', getAllBook);
+// Route pour créer un nouveau livre (avec authentification et gestion des fichiers)
 router.post('/', auth, multer, createBook);
+// Route pour modifier un livre existant (avec authentification et gestion des fichiers)
 router.put('/:id', auth, multer, modifyBook);
+// Route pour supprimer un livre (avec authentification)
 router.delete('/:id', auth, deleteBook);
+// Route pour ajouter une note à un livre (avec authentification)
 router.post('/:id/rating', auth, createRatingBook);
 
+// Exportation du routeur pour l'utiliser dans l'application principale
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); // Création du routeur Express
 
-const userCtrl = require('../controllers/user')
+const userCtrl = require('../controllers/user'); // Importation des fonctions du contrôleur utilisateur
 
+// Route pour l'inscription d'un nouvel utilisateur
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login)
+// Route pour la connexion d'un utilisateur existant
+router.post('/login', userCtrl.login);
 
+// Exportation du routeur pour l'utiliser dans l'application principale
 module.exports = router;
