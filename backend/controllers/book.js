0b80922e72f7921cd643a9ff41585b1fb7931ce0
Johnny Clarys <johnny.Clarys@hotmail.fr>
2024-07-20 07:36:04 +0200

    edit

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -4,74 +4,91 @@ const fs = require('fs');
 const path = require('path');
 const { cleanFileName, calculateAverageRating } = require('../functions/utils');
 
-// Function to retrieve all books from the database
+// Fonction pour récupérer tous les livres de la base de données
 exports.getAllBook = async (req, res, next) => {
     try {
+        // Récupère tous les livres
         const books = await Book.find();
+        // Réponse avec les livres récupérés
         res.status(200).json(books);
     } catch (error) {
+        // Réponse en cas d'erreur
         res.status(500).json({ error });
     }
 };
 
-// Function to retrieve the 3rd top-rated books from the database
+// Fonction pour récupérer les 3 livres les mieux notés de la base de données
 exports.getBestBooks = async (req, res, next) => {
     try {
+        // Récupère les trois livres avec les meilleures notes
         const bestBooks = await Book.find().sort({ averageRating: -1 }).limit(3);
+        // Réponse avec les livres récupérés
         res.status(200).json(bestBooks);
     } catch (error) {
+        // Réponse en cas d'erreur
         res.status(500).json({ error });
     }
 };
 
-// Function to retrieve a single book from the database based on its ID
+// Fonction pour récupérer un livre spécifique de la base de données en fonction de son ID
 exports.getOneBook = async (req, res, next) => {
     try {
+        // Récupère un livre par son ID
         const book = await Book.findOne({ _id: req.params.id });
 
         if (!book) {
+            // Réponse si le livre n'est pas trouvé
             res.status(404).json({ message: "Book not found." });
         } else {
+            // Réponse avec le livre récupéré
             res.status(200).json(book);
         }
-    } catch (error) {s
+    } catch (error) {
+        // Réponse en cas d'erreur
         res.status(500).json({ error });
     }
 };
 
-// Function to create a new book in the database
+// Fonction pour créer un nouveau livre dans la base de données
 exports.createBook = async (req, res, next) => {
     try {
-    const bookObject = JSON.parse(req.body.book);
-    delete bookObject._id;
-    delete bookObject.userId;
+        // Parse les données du livre à partir de la requête
+        const bookObject = JSON.parse(req.body.book);
+        delete bookObject._id;
+        delete bookObject.userId;
 
-    const buffer = req.file.buffer;
-    const timestamp = Date.now();
-    const name = cleanFileName(req.file.originalname);
-    const ref = `${timestamp}-${name.split('.')[0]}.webp`;
-    
-    // Convert and save image using sharp
-    await sharp(buffer)
-        .webp({ quality: 20 })
-        .toFile(path.join(__dirname, '../images', ref))
-        
+        // Récupère l'image du livre
+        const buffer = req.file.buffer;
+        const timestamp = Date.now();
+        const name = cleanFileName(req.file.originalname);
+        const ref = `${timestamp}-${name.split('.')[0]}.webp`;
+
+        // Convertit et enregistre l'image en utilisant sharp
+        await sharp(buffer)
+            .webp({ quality: 20 })
+            .toFile(path.join(__dirname, '../images', ref));
+
+        // Crée un nouvel objet livre
         const book = new Book({
             ...bookObject,
             userId: req.auth.userId,
             imageUrl: `${req.protocol}://${req.get('host')}/images/${ref}`
         });
 
+        // Enregistre le livre dans la base de données
         await book.save();
-        res.status(201).json({ message: 'Book created !' });
+        // Réponse de succès
+        res.status(201).json({ message: 'Book created!' });
     } catch (error) {
+        // Réponse en cas d'erreur
         res.status(500).json({ error });
     }
 };
 
-// Function to modify a book in the database
+// Fonction pour modifier un livre dans la base de données
 exports.modifyBook = async (req, res, next) => {
     try {
+        // Prépare les données du livre à partir de la requête
         const bookObject = req.file ? {
             ...JSON.parse(req.body.book),
             imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
@@ -80,13 +97,16 @@ exports.modifyBook = async (req, res, next) => {
         };
 
         delete bookObject.userId;
+
+        // Récupère le livre par son ID
         const book = await Book.findOne({ _id: req.params.id });
 
         if (book.userId != req.auth.userId) {
+            // Réponse si l'utilisateur n'est pas autorisé à modifier le livre
             return res.status(401).json({ message: 'Unauthorized.' });
         }
 
-        // If a new file is uploaded, process and save the image
+        // Si un nouveau fichier est téléchargé, traite et enregistre l'image
         if (req.file) {
             const buffer = req.file.buffer;
             const timestamp = Date.now();
@@ -97,10 +117,10 @@ exports.modifyBook = async (req, res, next) => {
                 .webp({ quality: 20 })
                 .toFile(path.join(__dirname, '../images', ref));
 
-            // Update imageUrl in bookObject
+            // Met à jour l'URL de l'image dans l'objet livre
             bookObject.imageUrl = `${req.protocol}://${req.get('host')}/images/${ref}`;
 
-            // If the book had an existing image, delete it
+            // Si le livre avait une image existante, la supprime
             if (book.imageUrl) {
                 const filename = book.imageUrl.split('/images/')[1];
                 fs.unlink(`images/${filename}`, (error) => {
@@ -111,49 +131,62 @@ exports.modifyBook = async (req, res, next) => {
             }
         }
 
+        // Met à jour le livre dans la base de données
         await Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id });
+        // Réponse de succès
         res.status(200).json({ message: 'Book modified!' });
     } catch (error) {
+        // Réponse en cas d'erreur
         res.status(500).json({ error });
     }
 };
 
-// Function to delete a book from the database
+// Fonction pour supprimer un livre de la base de données
 exports.deleteBook = async (req, res, next) => {
     try {
+        // Récupère le livre par son ID
         const book = await Book.findOne({ _id: req.params.id });
 
         if (book.userId != req.auth.userId) {
+            // Réponse si l'utilisateur n'est pas autorisé à supprimer le livre
             return res.status(401).json({ message: 'Unauthorized.' });
         }
 
         const filename = book.imageUrl.split('/images/')[1];
+        // Supprime l'image associée au livre
         fs.unlink(`images/${filename}`, () => {
+            // Supprime le livre de la base de données
             Book.deleteOne({ _id: req.params.id })
                 .then(() => res.status(200).json({ message: 'Book deleted!' }))
                 .catch(error => res.status(401).json({ error }));
         });
     } catch (error) {
+        // Réponse en cas d'erreur
         res.status(500).json({ error });
     }
 };
 
-// Function to add a rating for a book in the database
+// Fonction pour ajouter une note à un livre dans la base de données
 exports.createRatingBook = async (req, res, next) => {
     const userId = req.auth.userId;
     const rating = req.body.rating;
     try {
+        // Récupère le livre par son ID
         const book = await Book.findOne({ _id: req.params.id });
-        
+
         if (!book) {
+            // Réponse si le livre n'est pas trouvé
             return res.status(404).json({ message: "Book not found." });
         }
 
+        // Vérifie si l'utilisateur a déjà noté le livre
         const existingRating = book.ratings.find(r => r.userId === userId);
         if (existingRating) {
+            // Réponse si l'utilisateur a déjà noté le livre
             return res.status(400).json({ message: "You have already rated this book." });
         }
 
+        // Ajoute la nouvelle note
         const newRating = { userId: userId, grade: rating };
 
         const updatedBook = await Book.findOneAndUpdate(
@@ -162,13 +195,15 @@ exports.createRatingBook = async (req, res, next) => {
             { new: true }
         );
 
-        // Calculate and update the book's average rating
+        // Calcule et met à jour la note moyenne du livre
         const newAverageRating = calculateAverageRating(updatedBook.ratings);
         updatedBook.averageRating = newAverageRating;
         await updatedBook.save();
 
+        // Réponse avec le livre mis à jour
         res.status(200).json(updatedBook);
     } catch (error) {
+        // Réponse en cas d'erreur
         res.status(500).json({ error });
     }
 };
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,29 +3,36 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const { secretKey } = require('../config');
 
+// Fonction pour l'inscription d'un nouvel utilisateur
 exports.signup = (req, res, next) => {
-    
+    // Vérifie si l'email est valide
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const isValidEmail = emailRegex.test(req.body.email);
     
     if (!isValidEmail) {
+        // Réponse si l'email n'est pas valide
         return res.status(400).json({ message: 'Invalid email address.' });
     }
 
+    // Vérifie si le mot de passe est valide
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/;
     const isValidPassword = passwordRegex.test(req.body.password);
 
     if (!isValidPassword) {
+        // Réponse si le mot de passe n'est pas valide
         return res.status(400).json({ message: 'Password must contain at least 8 characters, including: 1 uppercase letter, 1 lowercase letter and 1 special character.'});
     }
 
+    // Hash le mot de passe
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
+        // Crée un nouvel utilisateur avec le mot de passe hashé
         const user = new User({
             email: req.body.email,
             password: hash,
             createdAt: new Date(),
         });
+        // Enregistre l'utilisateur dans la base de données
         user.save()
             .then(() => res.status(201).json({ message: 'User created' }))
             .catch(error => res.status(400).json({ error }));
@@ -33,38 +40,48 @@ exports.signup = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 };
 
+// Fonction pour la connexion d'un utilisateur
 exports.login = (req, res, next) => {
+    // Trouve l'utilisateur par email
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user === null) {
+                // Réponse si l'utilisateur n'est pas trouvé
                 res.status(401).json({ message: 'Incorrect email/password pair.' });
             } else {
+                // Compare le mot de passe fourni avec le mot de passe hashé stocké
                 bcrypt.compare(req.body.password, user.password)
                     .then(valid => {
                         if (!valid) {
+                            // Réponse si le mot de passe est incorrect
                             res.status(401).json({ message: 'Incorrect email/password pair.' });
-                        } 
-                        const lastActivity = user.lastActivity = Date.now();
-                        user.save()
-                            .then(() => {
-                                res.status(200).json({
-                                    userId: user._id,
-                                    lastActivity: lastActivity,
-                                    token: jwt.sign(
-                                        { userID: user._id },
-                                        secretKey,
-                                        { expiresIn: '24h' }
-                                    )
-                                });
-                            })
-                            .catch(error => res.status(500).json({ error: error.message }));
+                        } else {
+                            // Met à jour la dernière activité de l'utilisateur
+                            const lastActivity = user.lastActivity = Date.now();
+                            user.save()
+                                .then(() => {
+                                    // Réponse avec l'ID de l'utilisateur, la dernière activité et le token JWT
+                                    res.status(200).json({
+                                        userId: user._id,
+                                        lastActivity: lastActivity,
+                                        token: jwt.sign(
+                                            { userID: user._id },
+                                            secretKey,
+                                            { expiresIn: '24h' }
+                                        )
+                                    });
+                                })
+                                .catch(error => res.status(500).json({ error: error.message }));
+                        }
                     })
                     .catch(error => {
+                        // Réponse en cas d'erreur de comparaison de mot de passe
                         res.status(500).json({ error });
                     });
             }
         })
         .catch(error => {
+            // Réponse en cas d'erreur de recherche d'utilisateur
             res.status(500).json({ error });
         });
 };
