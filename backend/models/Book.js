6410b4b47b9a5d95e792c686d7da22ec7681ad5c
Johnny Clarys <johnny.Clarys@hotmail.fr>
2024-07-20 07:46:33 +0200

    edit Models

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,19 +1,32 @@
 const mongoose = require("mongoose");
 
+// Définition du schéma de livre pour Mongoose
 const bookSchema = mongoose.Schema({
+    // ID de l'utilisateur qui a ajouté le livre
     userId : { type: String, required: true },
+    // Titre du livre
     title: { type: String, required: true },
+    // Auteur du livre
     author : { type: String, required: true },
+    // URL de l'image du livre
     imageUrl : { type: String, required: true },
+    // Année de publication du livre
     year: { type: Number, required: true },
+    // Genre du livre
     genre: { type: String, required: true },
+    // Tableau de notes données par les utilisateurs
     ratings: [{
+        // ID de l'utilisateur qui a donné la note
         userId: { type: String, required: true },
+        // Note donnée par l'utilisateur (de 0 à 5)
         grade: { type: Number, required: true, min: 0, max: 5 }
     }],
-    averageRating: { type: Number, default: 0, required: true,  min: 0, max: 5 }
+    // Note moyenne du livre (de 0 à 5)
+    averageRating: { type: Number, default: 0, required: true, min: 0, max: 5 }
 });
 
+// Création du modèle Book à partir du schéma bookSchema
 const Book = mongoose.model('Book', bookSchema);
 
+// Exportation du modèle Book
 module.exports = Book;
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,23 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+// Définition du schéma d'utilisateur pour Mongoose
 const userSchema = mongoose.Schema({
+  // Adresse email de l'utilisateur (doit être unique)
   email: { type: String, required: true, unique: true },
+  // Mot de passe de l'utilisateur
   password: { type: String, required: true },
+  // Date de création du compte utilisateur
   createdAt: { type: Date },
-  lastActivity: { type: Date  }
+  // Dernière activité de l'utilisateur
+  lastActivity: { type: Date }
 });
 
+// Applique le plugin uniqueValidator au schéma d'utilisateur pour garantir l'unicité de l'email
 userSchema.plugin(uniqueValidator);
 
+// Création du modèle User à partir du schéma userSchema
 const User = mongoose.model('User', userSchema);
 
+// Exportation du modèle User
 module.exports = User;
