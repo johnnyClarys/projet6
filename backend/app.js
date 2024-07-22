1399482f98fa5c5126d2b0dd04b18bfa6bf03e93
Johnny Clarys <johnny.Clarys@hotmail.fr>
2024-07-22 10:59:15 +0200

    edit des fichiers app.js, config.js....

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,30 @@ const express = require('express');
 const path = require('path');
 const connectDB = require('./db'); // Fonction pour se connecter à la base de données MongoDB
 const corsConfig = require('./corsConfig'); // Configuration des en-têtes CORS
-
 const bookRoutes = require('./routes/book'); // Routes pour les livres
 const userRoutes = require('./routes/user'); // Routes pour les utilisateurs
-
 const app = express(); // Création de l'application Express
+
+
+
+require('dotenv').config(); // Charge les variables d'environnement depuis le fichier .env
+const jwt = require('jsonwebtoken'); // Importe le module jsonwebtoken
+const secretKey = process.env.SECRET_KEY; // Récupère la clé secrète depuis les variables d'environnement
+if (!secretKey) {
+  console.error('SECRET_KEY is not defined'); // Affiche une erreur si la clé secrète n'est pas définie
+  process.exit(1); // Quitte le processus si la clé secrète n'est pas définie
+}
+const payload = { userId: 123, username: 'exampleUser' }; // Charge utile du jeton
+const token = jwt.sign(payload, secretKey, { expiresIn: '1h' }); // Génère un jeton JWT avec une expiration d'une heure
+console.log('Generated Token:', token); // Affiche le jeton généré
+const tokenToVerify = token; // Jeton à vérifier
+try {
+  const decoded = jwt.verify(tokenToVerify, secretKey); // Vérifie et décode le jeton
+  console.log('Decoded Token:', decoded); // Affiche le jeton décodé
+} catch (err) {
+  console.error('Invalid Token:', err.message); // Affiche une erreur si le jeton est invalide
+}
+
 
 // Connexion à MongoDB
 connectDB();
--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,8 @@
 // Utilisez ce fichier pour gérer les configurations sensibles de l'application
 // Pour générer une clé secrète sécurisée, décommentez les lignes suivantes :
 
-// const crypto = require('crypto');
-// const secretKey = crypto.randomBytes(32).toString('hex');
+const crypto = require('crypto');
+const secretKey = crypto.randomBytes(32).toString('hex');
 
 // Assurez-vous que la clé secrète est définie dans les variables d'environnement
 if (!process.env.SECRET_KEY) {
