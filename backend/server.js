86b3a04d37fe42fee5e1f0e4a5034daf771b7a74
Johnny Clarys <johnny.Clarys@hotmail.fr>
2024-07-25 11:29:29 +0200

    edit server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,48 @@
-const http = require('node:http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
 
-// Définir le port sur lequel le serveur va écouter
-const port = process.env.PORT || 3000;
-app.set('port', port);
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
-// Créer le serveur HTTP en utilisant l'application Express
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "4000");
+app.set("port", port);
+
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
 const server = http.createServer(app);
 
-// Démarrer le serveur et écouter sur le port défini
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
+});
 
-server.listen(process.env.PORT || 3000);
+server.listen(port);
